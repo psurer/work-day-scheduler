4cdafbbf3d63b09e1098321021deb3f21b4614b7
psurer <surereker@icloud.com>
2021-04-10 20:47:28 -0400

    solution for homework

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,32 +1,36 @@
 // I need to collection of buss hours 9-5
 
-var bizHours  = [];
+var bussHours  = [];
 var isEditing = false;
+const timeFormat = "DD:MM:YYYY";
+var currentTime = moment().format(timeFormat);
+var schedulerData = [];  // data to be saved in localstorage
 
-// populating the array
 
+// populating the scheduler table
 function setupScheduler(){
+    let rows = '';
     for (let index = 0, MAX_HOURS = 9; index < 9; index++) {
-        bizHours[index] = index + MAX_HOURS; 
-        $('#scheduler').append(`
-            <tr class="row" onclick="scheduleAnEvent(event,'id-for-${bizHours[index]}');">
-                <td class="hour"> ${bizHours[index]}</td>
-                <td class='event ${applyColor(bizHours[index])}'> 
-                    <span id='id-for-${bizHours[index]}'>EVENT</span>
+        bussHours[index] = index + MAX_HOURS;
+        rows += `
+            <tr class="row">
+                <td class="hour"> ${bussHours[index]}:00</td>
+                <td class='event ${applyColor(index + MAX_HOURS)}' onclick="scheduleAnEvent(event,'id-for-${bussHours[index]}')"> 
+                    <span id='id-for-${bussHours[index]}'></span>
                 </td>
-                <td class="action"><button onclick="saveEvent('id-for-${bizHours[index]}')">Save</button></td>
-            </tr>`);
+                <td class="action"><button onclick="saveEvent('id-for-${bussHours[index]}')">Save</button></td>
+            </tr>`;
     }
+    $('#scheduler').append(rows);
 }
 // Apply colors to scheduler
 // We need to pass an hour to this function
 // a based on the time ( hour ) pass, we will apply the
 // correct color.
 function applyColor(toEventColumnForHour){
-    const currentTime = 15;
-    if ( currentTime < toEventColumnForHour) {
+    if (toEventColumnForHour <= moment().hour() ) {
         return 'past';
-    } else if (currentTime > toEventColumnForHour){
+    } else if (moment().hour() >= toEventColumnForHour){
         return 'future';
     }
     return  'present';
@@ -35,54 +39,44 @@ function applyColor(toEventColumnForHour){
 
 // Create event for user
 function scheduleAnEvent(event, timePicked){
-    if ( isEditing === false) {
-        isEditing = true;
-        $(`#${timePicked}`).append('<input type="text" class="inputEvent"/>');
+    event.preventDefault();
+    event.stopPropagation();
+    if ( $(`#${timePicked} input`).length === 0) {
+        $(`#${timePicked}`).append('<input type="text" class="inputEvent"/>').focus();
     }
 }
 
 // Save a new event
 function saveEvent(timePicked) {
     const newEvent = $('input').val();
-    saveDataOnLocalStorage({ time: 15, event: newEvent});
+    saveDataOnLocalStorage({ time: timePicked, event: newEvent});
     $('tr input').remove();
-    $(`#${timePicked}`).text(newEvent);
+    $(`#${timePicked}`).text(newEvent);currentTime
     isEditing = false;
 }
 
+//Display current time.
+function displayCurrentTime(time){
+    $('#currentDay').text(time);
+}
+
+// Main function
 function main(){
+    displayCurrentTime(currentTime);
     setupScheduler();
-
 }
 
+
+//Save data to local storage
 function saveDataOnLocalStorage(data) {
-    localStorage.setItem(data.time, data.event);
+    const item = schedulerData.find( (item) => item.time == data.time );
+    if (item) {
+        item.event = data.event;
+    } else {
+        schedulerData.push ({data})
+    }
+    localStorage.setItem('scheduler', schedulerData);
 }
+
 
 main();
- 
- displayTime.textContent = currentTime.format("dddd, MMMM Do")
- 
- $(".save-btn").on("click", function() {
-     var value = $(this).siblings(".time-block-text").val();
-     var time = $(this).parent().attr("id");
-     localStorage.setItem(time, value);
- });
-
-$("#row-900 .time-block-text").val(localStorage.getItem("row-900" /* localStorage KEY also the row id */));
-
-$("#row-1000 .time-block-text").val(localStorage.getItem("row-1000" /* localStorage KEY also the row id */));
-
-$("#row-1100 .time-block-text").val(localStorage.getItem("row-1100" /* localStorage KEY also the row id */));
-
-$("#row-1200 .time-block-text").val(localStorage.getItem("row-1200" /* localStorage KEY also the row id */));
-
-$("#row-1300 .time-block-text").val(localStorage.getItem("row-1300" /* localStorage KEY also the row id */));
-
-$("#row-1400 .time-block-text").val(localStorage.getItem("row-1400" /* localStorage KEY also the row id */));
-
-$("#row-1500 .time-block-text").val(localStorage.getItem("row-1500" /* localStorage KEY also the row id */));
-
-$("#row-1600.time-block-text").val(localStorage.getItem("row-1600"/* localStorage KEY also the row id */));
-
-$("#row-1700 .time-block-text").val(localStorage.getItem("row-1700" /* localStorage KEY also the row id */));
